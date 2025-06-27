33edcb04130e10c6723646393b1401e45515e9ae
MaRuF2131 <marufahmmed916@gmail.com>
2025-06-28 01:41:10 +0600

    number and password validation check

--- src/components/custom-validation/CustomValidation.jsx
+++ src/components/custom-validation/CustomValidation.jsx
@@ -22,3 +22,84 @@ export const StringValidationCheck = {
     );
   },
 };
+
+const NumberValidationCheck={
+  validate:async(v)=>{
+    return await new Promise(
+      (resolve)=>{
+        setTimeout(()=>{
+            const num = parseFloat(v);
+
+              if (isNaN(num)) {
+                return resolve("Must be a valid number");
+              }
+
+              if (num < 1) {
+                return resolve("Must be at least 1");
+              }
+
+              if (num > 100) {
+                return resolve("Cannot exceed 100");
+              }
+
+              if (!Number.isInteger(num)) {
+                return resolve("Only whole numbers allowed");
+              }
+
+              resolve(true); //  Passed all checks
+        },500)
+      }
+    )
+  }
+}
+
+// return multiple error together 
+const PasswordValidationCheck={
+   validate:{
+      islength:async(v)=>{
+        return await Promise((resolve)=>setTimeout(()=>{
+            if(v.lenght<6){
+              resolve("At least 6 character need");
+            }else{
+              resolve(true);
+            }
+        },500))
+      },
+      isuppercase:async(v)=>{
+        return await new Promise((resolve)=>setTimeout(()=>{
+            if(!/[A-Z]/.test(v)){
+              resolve("At least one uppercase letter need");
+            }else{
+              resolve(true);
+            }
+        },500))
+      },
+      isspacial:async(v)=>{
+        return await new Promise((resolve)=>setTimeout(()=>{
+            if(!/[!@#$%^&*(),.?:{}|]/.test(v)){
+              resolve("At least one special character need");
+            }else{
+              resolve(true);
+            }
+        },500))
+      },
+      isdigit:async(v)=>{
+        return await new Promise((resolve)=>setTimeout(()=>{
+            if(!/[\d]/.test(v)){
+              resolve("At least one digit need");
+            }else{
+              resolve(true);
+            }
+        },500))
+      },
+      isdangerous:async(v)=>{
+        return await new Promise((resolve)=>setTimeout(()=>{
+            if(StringValidationCheck.validate(v) !== true){
+              resolve("Dangerous content detected");
+            }else{
+              resolve(true);
+            }
+        },500))
+      }
+   }
+}
